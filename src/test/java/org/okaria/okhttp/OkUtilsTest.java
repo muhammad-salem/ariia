dbcbc2281d5f0f00264a27676f224346c5961bc8
salemebo <salem.ebo@gmail.com>
2020-03-24 19:50:04 +0200

    remove unused import

--- src/test/java/org/okaria/okhttp/OkUtilsTest.java
+++ src/test/java/org/okaria/okhttp/OkUtilsTest.java
@@ -1,19 +1,5 @@
 package org.okaria.okhttp;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
-import okhttp3.Cookie;
-import okhttp3.HttpUrl;
-
 import org.junit.Test;
 
 public class OkUtilsTest {
