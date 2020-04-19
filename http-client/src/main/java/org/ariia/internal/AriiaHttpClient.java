08ad1d1fae4b4e55e40308dd40456986f59be402
salemebo <salem.ebo@gmail.com>
2020-04-19 13:36:14 +0200

    set max jdk/jdm version 11

--- http-client/src/main/java/org/ariia/internal/AriiaHttpClient.java
+++ http-client/src/main/java/org/ariia/internal/AriiaHttpClient.java
@@ -82,48 +82,48 @@ public class AriiaHttpClient implements ClientRequest {
 	}
 
 	private String getStateCodeMessage(int statusCode) {
-		return switch (statusCode) {
-		case 100: { yield "Continue"; }
-		case 101: { yield "Switching Protocols"; }
-		case 200: { yield "OK"; }
-		case 201: { yield "Created"; }
-		case 202: { yield "Accepted"; }
-		case 203: { yield "Non-authoritative Information"; }
-		case 204: { yield "No Content"; }
-		case 205: { yield "Reset Content"; }
-		case 206: { yield "Partial Content"; }
-		case 300: { yield "Multiple Choices"; }
-		case 301: { yield "Moved Permanently"; }
-		case 302: { yield "Found"; }
-		case 303: { yield "See Other"; }
-		case 304: { yield "Not Modified"; }
-		case 305: { yield "Use Proxy"; }
-		case 306: { yield "Unused"; }
-		case 307: { yield "Temporary Redirect"; }
-		case 400: { yield "Bad Request"; }
-		case 401: { yield "Unauthorized"; }
-		case 402: { yield "Payment Required"; }
-		case 403: { yield "Forbidden"; }
-		case 404: { yield "Not Found"; }
-		case 405: { yield "Method Not Allowed"; }
-		case 406: { yield "Not Acceptable"; }
-		case 407: { yield "Proxy Authentication Required"; }
-		case 408: { yield "Request Timeout"; }
-		case 409: { yield "Conflict"; }
-		case 410: { yield "Gone"; }
-		case 411: { yield "Length Required"; }
-		case 412: { yield "Precondition Failed"; }
-		case 413: { yield "Request Entity Too Large"; }
-		case 414: { yield "Request-url Too Long"; }
-		case 415: { yield "Unsupported Media Type"; }
-		case 417: { yield "Expectation Failed"; }
-		case 500: { yield "Internal Server Error"; }
-		case 501: { yield "Not Implemented"; }
-		case 502: { yield "Bad Gateway"; }
-		case 503: { yield "Service Unavailable"; }
-		case 504: { yield "Gateway Timeout"; }
-		case 505: { yield "HTTP Version Not Supported"; }
-		default: yield "";};
+		switch (statusCode) {
+		case 100: { return "Continue"; }
+		case 101: { return "Switching Protocols"; }
+		case 200: { return "OK"; }
+		case 201: { return "Created"; }
+		case 202: { return "Accepted"; }
+		case 203: { return "Non-authoritative Information"; }
+		case 204: { return "No Content"; }
+		case 205: { return "Reset Content"; }
+		case 206: { return "Partial Content"; }
+		case 300: { return "Multiple Choices"; }
+		case 301: { return "Moved Permanently"; }
+		case 302: { return "Found"; }
+		case 303: { return "See Other"; }
+		case 304: { return "Not Modified"; }
+		case 305: { return "Use Proxy"; }
+		case 306: { return "Unused"; }
+		case 307: { return "Temporary Redirect"; }
+		case 400: { return "Bad Request"; }
+		case 401: { return "Unauthorized"; }
+		case 402: { return "Payment Required"; }
+		case 403: { return "Forbidden"; }
+		case 404: { return "Not Found"; }
+		case 405: { return "Method Not Allowed"; }
+		case 406: { return "Not Acceptable"; }
+		case 407: { return "Proxy Authentication Required"; }
+		case 408: { return "Request Timeout"; }
+		case 409: { return "Conflict"; }
+		case 410: { return "Gone"; }
+		case 411: { return "Length Required"; }
+		case 412: { return "Precondition Failed"; }
+		case 413: { return "Request Entity Too Large"; }
+		case 414: { return "Request-url Too Long"; }
+		case 415: { return "Unsupported Media Type"; }
+		case 417: { return "Expectation Failed"; }
+		case 500: { return "Internal Server Error"; }
+		case 501: { return "Not Implemented"; }
+		case 502: { return "Bad Gateway"; }
+		case 503: { return "Service Unavailable"; }
+		case 504: { return "Gateway Timeout"; }
+		case 505: { return "HTTP Version Not Supported"; }
+		default: return "";}
 	}
 
 	@Override
