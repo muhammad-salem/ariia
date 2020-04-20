fc15cd060a3feaec78590f72b69c5490c0c9b0e5
salemebo <salem.ebo@gmail.com>
2020-04-20 15:05:32 +0200

    resolve mime type from properties file

--- restful-api/src/main/java/org/ariia/mvc/resource/MimeType.java
+++ restful-api/src/main/java/org/ariia/mvc/resource/MimeType.java
@@ -1,22 +1,33 @@
 package org.ariia.mvc.resource;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+import java.util.Properties;
 
 public final class MimeType {
 	
-	private static Map<String, String> MIME_TYPE = new HashMap<>();
+	private static Properties properties;
+	
+//	private static Map<String, String> MIME_TYPE = new HashMap<>();
 	static {
-		String types = "css=text/css;htm=text/html;html=text/html;xml=text/xml;java=text/x-java-source, text/java;md=text/plain;txt=text/plain;asc=text/plain;gif=image/gif;jpg=image/jpeg;jpeg=image/jpeg;png=image/png;svg=image/svg+xml;mp3=audio/mpeg;m3u=audio/mpeg-url;mp4=video/mp4;ogv=video/ogg;flv=video/x-flv;mov=video/quicktime;swf=application/x-shockwave-flash;js=application/javascript;pdf=application/pdf;doc=application/msword;ogg=application/x-ogg;zip=application/octet-stream;exe=application/octet-stream;class=application/octet-stream;m3u8=application/vnd.apple.mpegurl;ts=video/mp2t;ico=image/x-icon";
-		String[] mimes = types.split(";");
-		for (String string : mimes) {
-			String[] temp = string.split("=");
-			MIME_TYPE.put(temp[0], temp[1]);
+//		String types = "css=text/css;htm=text/html;html=text/html;xml=text/xml;java=text/x-java-source, text/java;md=text/plain;txt=text/plain;asc=text/plain;gif=image/gif;jpg=image/jpeg;jpeg=image/jpeg;png=image/png;svg=image/svg+xml;mp3=audio/mpeg;m3u=audio/mpeg-url;mp4=video/mp4;ogv=video/ogg;flv=video/x-flv;mov=video/quicktime;swf=application/x-shockwave-flash;js=application/javascript;pdf=application/pdf;doc=application/msword;ogg=application/x-ogg;zip=application/octet-stream;exe=application/octet-stream;class=application/octet-stream;m3u8=application/vnd.apple.mpegurl;ts=video/mp2t;ico=image/x-icon";
+//		String[] mimes = types.split(";");
+//		for (String string : mimes) {
+//			String[] temp = string.split("=");
+//			MIME_TYPE.put(temp[0], temp[1]);
+//		}
+		properties = new Properties();
+		try {
+			properties.load(MimeType.class.getResourceAsStream("/mime-types.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	public static String mime(String type) {
-		return MIME_TYPE.get(type);
+		return properties.getProperty(type, "application/octet-stream");
+//		return MIME_TYPE.get(type);
 	}
 	
 	public static String getType(String fileName) {
