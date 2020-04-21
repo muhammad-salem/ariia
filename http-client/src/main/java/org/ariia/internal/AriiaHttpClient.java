7c0a43120239fe01ed8ee8504a8e48850e30c5c0
salemebo <salem.ebo@gmail.com>
2020-04-21 15:18:45 +0200

    create code class

--- http-client/src/main/java/org/ariia/internal/AriiaHttpClient.java
+++ http-client/src/main/java/org/ariia/internal/AriiaHttpClient.java
@@ -66,12 +66,13 @@ public class AriiaHttpClient implements ClientRequest {
 			HttpResponse<InputStream> response = 
 					client.send(requestBuilder.build(), BodyHandlers.ofInputStream());
 			
+			
 			Response.Builder responseBuilder = new Response.Builder();
 	        responseBuilder.code(response.statusCode());
 	        responseBuilder.requestUrl(response.uri().toString());
 	        responseBuilder.requestMethod(response.request().method());
 	        responseBuilder.protocol(response.version().toString());
-	        responseBuilder.responseMessage(getStateCodeMessage(response.statusCode()));
+	        responseBuilder.responseMessage(Code.msg(response.statusCode()));
 	        responseBuilder.headers(response.headers().map());
 	        responseBuilder.setBodyBytes(response.body());
 			return responseBuilder.build();
@@ -79,51 +80,6 @@ public class AriiaHttpClient implements ClientRequest {
 		} catch ( InterruptedException | URISyntaxException e) {
 			throw new IOException(e.getMessage(), e);
 		}
-	}
-
-	private String getStateCodeMessage(int statusCode) {
-		switch (statusCode) {
-		case 100: { return "Continue"; }
-		case 101: { return "Switching Protocols"; }
-		case 200: { return "OK"; }
-		case 201: { return "Created"; }
-		case 202: { return "Accepted"; }
-		case 203: { return "Non-authoritative Information"; }
-		case 204: { return "No Content"; }
-		case 205: { return "Reset Content"; }
-		case 206: { return "Partial Content"; }
-		case 300: { return "Multiple Choices"; }
-		case 301: { return "Moved Permanently"; }
-		case 302: { return "Found"; }
-		case 303: { return "See Other"; }
-		case 304: { return "Not Modified"; }
-		case 305: { return "Use Proxy"; }
-		case 306: { return "Unused"; }
-		case 307: { return "Temporary Redirect"; }
-		case 400: { return "Bad Request"; }
-		case 401: { return "Unauthorized"; }
-		case 402: { return "Payment Required"; }
-		case 403: { return "Forbidden"; }
-		case 404: { return "Not Found"; }
-		case 405: { return "Method Not Allowed"; }
-		case 406: { return "Not Acceptable"; }
-		case 407: { return "Proxy Authentication Required"; }
-		case 408: { return "Request Timeout"; }
-		case 409: { return "Conflict"; }
-		case 410: { return "Gone"; }
-		case 411: { return "Length Required"; }
-		case 412: { return "Precondition Failed"; }
-		case 413: { return "Request Entity Too Large"; }
-		case 414: { return "Request-url Too Long"; }
-		case 415: { return "Unsupported Media Type"; }
-		case 417: { return "Expectation Failed"; }
-		case 500: { return "Internal Server Error"; }
-		case 501: { return "Not Implemented"; }
-		case 502: { return "Bad Gateway"; }
-		case 503: { return "Service Unavailable"; }
-		case 504: { return "Gateway Timeout"; }
-		case 505: { return "HTTP Version Not Supported"; }
-		default: return "";}
 	}
 
 	@Override
