5aa33a9c7f54fe809c27a9e27c0f3e930b1e26a3
salemebo <salem.ebo@gmail.com>
2020-05-26 10:14:23 +0200

    add item meta data wrapper in case to use with complete

---
+++ core-api/src/main/java/org/ariia/core/api/writer/ItemMetaDataCompleteWarpper.java
@@ -0,0 +1,78 @@
+package org.ariia.core.api.writer;
+
+import org.ariia.core.api.queue.ItemDownloader;
+import org.ariia.items.Item;
+import org.ariia.segment.Segment;
+import org.ariia.speed.SpeedMonitor;
+
+public class ItemMetaDataCompleteWarpper extends ItemMetaData {
+
+	public ItemMetaDataCompleteWarpper(Item item) {
+		super(item);
+	}
+
+	@Override
+	public void releaseSegment(Segment segment) { }
+
+	@Override
+	protected void initMetaData() {}
+
+	@Override
+	protected boolean writeSegment(Segment segment) {return false;}
+
+	@Override
+	public void forceUpdate() {}
+
+	@Override
+	protected void initRandomAccessFile() {	}
+
+	@Override
+	public boolean isClose() { return super.isClose(); }
+
+	@Override
+	public boolean isOpen() {return false;}
+
+	@Override
+	public void close() {}
+
+	@Override
+	public void offerSegment(Segment segment) {	}
+
+	@Override
+	public synchronized void systemFlush() {
+	}
+
+	@Override
+	public int segmentSize() {
+		return 0;
+	}
+
+	@Override
+	public boolean isDownloading() {return false;}
+
+	@Override
+	public boolean allowSegmentWrite() {return false;}
+
+	@Override
+	public void checkCompleted() {}
+
+	@Override
+	public void checkWhileDownloading() {	}
+
+	@Override
+	public void pause() {}
+
+	@Override
+	public void initWaitQueue() {}
+
+	@Override
+	public boolean isDownloadListEmpty() {return true;}
+
+	@Override
+	public void startAndCheckDownloadQueue(ItemDownloader plane, SpeedMonitor... monitors) {	}
+
+	
+	
+	
+
+}
