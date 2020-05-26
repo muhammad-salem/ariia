56598b9ce3947afa0b37e2a7ddaeb4fb04635bb8
salemebo <salem.ebo@gmail.com>
2020-05-26 12:22:45 +0200

    create router RESTFul API

---
+++ restful-api/src/main/java/org/ariia/mvc/router/RESTFulContext.java
@@ -0,0 +1,114 @@
+package org.ariia.mvc.router;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+
+/**
+ * for RESTFulContext main could will be "/api/v1" or "/api/v2"
+ * to add router use pattern like as:
+ *  "/student/edit/:id"
+ *  
+ *  so the full URL to this service "/api/v2/student/edit/:id"
+ * @author salem
+ *
+ */
+public class RESTFulContext implements HttpHandler {
+	
+	protected String context;
+	protected HttpServer server;
+	
+	protected Set<RESTfulRouter> restfulRouters;
+	
+	
+	/**
+	 * use '/' as default constructor
+	 * @param server
+	 */
+	public RESTFulContext(HttpServer server) {
+		this("/", server);
+	}
+	
+	public RESTFulContext(String context, HttpServer server) {
+		this.context = Objects.requireNonNull(context);
+		this.server = Objects.requireNonNull(server);
+		this.server.createContext(context, this);
+		this.restfulRouters = new HashSet<>();
+	}
+	
+	/**
+	 * Subscriber Router
+	 * @param router
+	 * @param method
+	 * @return 
+	 */
+	protected RESTfulRouter subscribeRouter(String router, String method) {
+		String regex = '^' + router.replaceAll(":(?:.[a-zA-Z0-9]*)", "(?:.*)");
+		RESTfulRouter resTfulRouter = new RESTfulRouter(this, method ,router, regex);
+		restfulRouters.add(resTfulRouter);
+		return resTfulRouter;
+	}
+	
+	public RESTfulRouter get(String router) {
+		return subscribeRouter(router, "GET");
+	}
+	
+	public RESTfulRouter head(String router) {
+		return subscribeRouter(router, "HEAD");
+	}
+	
+	public RESTfulRouter post(String router) {
+		return subscribeRouter(router, "POST");
+	}
+	
+	public RESTfulRouter put(String router) {
+		return subscribeRouter(router, "PUT");
+	}
+	
+	public RESTfulRouter delete(String router) {
+		return subscribeRouter(router, "DELETE");
+	}
+
+	public RESTfulRouter patch(String router) {
+		return subscribeRouter(router, "PATCH");
+	}
+	
+	public RESTfulRouter options(String router) {
+		return subscribeRouter(router, "OPTIONS");
+	}
+	
+	public RESTfulRouter trace(String router) {
+		return subscribeRouter(router, "TRACE");
+	}
+	
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		
+		String uri = exchange.getRequestURI().toString();
+		String contextPath = uri.replaceFirst(context, "");
+		Stream<RESTfulRouter> stream = restfulRouters.stream();
+		stream.filter(restful -> {
+			return exchange.getRequestMethod().equalsIgnoreCase(restful.getMethod()) || exchange.getRequestMethod().equalsIgnoreCase("*");
+		})
+		.filter(restful -> {
+			return contextPath.matches(restful.getRegex());
+		})
+		.limit(1l)
+		.forEach(restful -> {
+			try {
+				restful.getHandler().handle(exchange);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+	
+
+}
---
+++ restful-api/src/main/java/org/ariia/mvc/router/RESTfulRouter.java
@@ -0,0 +1,55 @@
+package org.ariia.mvc.router;
+
+import java.util.Objects;
+import com.sun.net.httpserver.HttpHandler;
+
+public class RESTfulRouter {
+	
+	String method;
+	String router;
+	String regex;
+	
+	RESTFulContext restFulContext;
+	
+	HttpHandler handler = (exchange) -> {exchange.close();};
+	
+	public RESTfulRouter(RESTFulContext restFulContext, String method, String router, String regex) {
+		this.restFulContext = Objects.requireNonNull(restFulContext);
+		this.method = Objects.requireNonNull(method);
+		this.router = Objects.requireNonNull(router);
+		this.regex  = Objects.requireNonNull(regex);
+//		this.handlers = new HashSet<>();
+	}
+	public String getMethod() {
+		return method;
+	}
+	public String getRouter() {
+		return router;
+	}
+	public String getRegex() {
+		return regex;
+	}
+	public HttpHandler getHandler() {
+		return handler;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof RESTfulRouter)) {
+			return false;
+		}
+		RESTfulRouter other = (RESTfulRouter) obj;
+		return regex.equals(other.regex);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(method, router, regex);
+	}
+	
+	public RESTFulContext subscribe(HttpHandler handler) {
+		this.handler = handler;
+		return restFulContext;
+	}
+	
+}
