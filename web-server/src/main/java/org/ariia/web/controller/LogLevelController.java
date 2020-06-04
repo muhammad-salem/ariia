566a6261cc29ee8881125f538d7be1483035193d
salemebo <salem.ebo@gmail.com>
2020-06-04 14:11:11 +0200

    get all values of log level

--- web-server/src/main/java/org/ariia/web/controller/LogLevelController.java
+++ web-server/src/main/java/org/ariia/web/controller/LogLevelController.java
@@ -3,18 +3,29 @@ package org.ariia.web.controller;
 import org.ariia.logging.Log;
 import org.ariia.mvc.annotation.RequestBody;
 import org.ariia.mvc.annotation.RestController;
+import org.ariia.mvc.annotation.method.GetRequest;
 import org.ariia.mvc.annotation.method.PostRequest;
 import org.terminal.console.log.Level;
 
 @RestController("/logging")
 public class LogLevelController {
 	
+	
+	@GetRequest(path = "/")
+	public Level getLevel() {
+		return Log.getLevel();
+	}
 		
 	@PostRequest(path = "/set")
 	public boolean setLevel(@RequestBody String levelName) {
 		Level level = Level.valueOf(levelName);
 		Log.level(level);
 		return true;
+	}
+	
+	@GetRequest(path = "/values")
+	public Level[] values() {
+		return Level.values();
 	}
 
 }
