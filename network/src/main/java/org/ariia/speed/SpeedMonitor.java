fef968b6e13e8af1f84ce87ec02e6516ca02cb34
salemebo <salem.ebo@gmail.com>
2020-06-12 20:17:54 +0200

    make old date transient so it can't be serialized

--- network/src/main/java/org/ariia/speed/SpeedMonitor.java
+++ network/src/main/java/org/ariia/speed/SpeedMonitor.java
@@ -7,77 +7,57 @@ import org.ariia.speed.report.SpeedReport;
 
 public class SpeedMonitor extends NetworkMonitor implements SocketMonitor, DatagramSocketMonitor, SpeedReport {
 
-	protected long receiveTCP_old = 0;
-	protected long receiveUDP_old = 0;
-	protected long sendTCP_old = 0;
-	protected long sendUDP_old = 0;
-
-	@Override
-	public long getTotalReceive() {
-		return receiveTCP + receiveUDP;
-	}
-
-	@Override
-	public long getTotalSend() {
-		return sendTCP + sendUDP;
-	}
-
-	@Override
-	public long getTotal() {
-		return getTotalReceive() + getTotalSend();
-	}
-
-	@Override
-	public long getReceiveTCP() {
-		return receiveTCP;
-	}
-
-	@Override
-	public long getReceiveUDP() {
-		return receiveUDP;
-	}
-
-	@Override
-	public long getSendTCP() {
-		return sendTCP;
-	}
-
-	@Override
-	public long getSendUDP() {
-		return sendUDP;
-	}
+	protected transient long receiveTCP_old = 0;
+	protected transient long receiveUDP_old = 0;
+	protected transient long sendTCP_old = 0;
+	protected transient long sendUDP_old = 0;
+	
+	
+	protected long speedOfTCPReceive = 0;
+	protected long speedOfTCPSend = 0;
+	
+	protected long speedOfUDPReceive = 0;
+	protected long speedOfUDPSend = 0;
 
 	/**
 	 * Call this method to snapshot old vales of Send and Received of TCP and UDP transfer.
 	 */
 	@Override
-	public void demondSpeedNow() {
+	public void snapshotLength() {
 		receiveTCP_old = receiveTCP;
 		sendTCP_old = sendTCP;
 		sendUDP_old = sendUDP;
 		receiveUDP_old = receiveUDP;
 	}
-
+	
 	@Override
 	public long speedOfTCPReceive() {
-		return receiveTCP - receiveTCP_old;
+		return speedOfTCPReceive;
 	}
 
 	@Override
 	public long speedOfUDPReceive() {
-		return receiveUDP - receiveUDP_old;
+		return speedOfUDPReceive;
 	}
 
 	@Override
 	public long speedOfTCPSend() {
-		return sendTCP - sendTCP_old;
+		return speedOfTCPSend;
 	}
 
 	@Override
 	public long speedOfUDPSend() {
-		return sendUDP - sendUDP_old;
+		return speedOfUDPSend;
 	}
-
+	
+	@Override
+	public void snapshotSpeed() {
+		speedOfTCPReceive  = receiveTCP - receiveTCP_old;
+		speedOfUDPReceive  = receiveUDP - receiveUDP_old;
+		speedOfTCPSend = sendTCP - sendTCP_old;
+		speedOfUDPSend = sendUDP - sendUDP_old;
+	}
+	
 	@Override
 	public String toString() {
 		return "SpeedMonitor: {" + "sendTCP: " + sendTCP + ", receiveTCP: " + receiveTCP + ", sendUDP: " + sendUDP
