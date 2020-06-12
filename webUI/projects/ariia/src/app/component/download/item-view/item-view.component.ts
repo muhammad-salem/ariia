7415a196f936c697e670a961877d64397a6d0453
salemebo <salem.ebo@gmail.com>
2020-06-12 09:18:31 +0200

    update ItemViewComponent
    - show file size
    - add svg for binding range arrays to show which range is finished
    - add circle progress bar

--- webUI/projects/ariia/src/app/component/download/item-view/item-view.component.ts
+++ webUI/projects/ariia/src/app/component/download/item-view/item-view.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Item } from '../../../model/item';
 import { RangeInfoService } from '../../../service/range-info.service';
 import { ItemService } from '../../../service/item.service';
-import { faTrash, faDownload, faPauseCircle, faCloudDownloadAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faDownload, faPauseCircle, faCloudDownloadAlt, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -10,7 +10,7 @@ import { faTrash, faDownload, faPauseCircle, faCloudDownloadAlt } from '@fortawe
   templateUrl: './item-view.component.html',
   styleUrls: ['./item-view.component.scss'],
   providers: [
-    {provide: RangeInfoService}
+    { provide: RangeInfoService }
   ]
 })
 export class ItemViewComponent implements OnInit {
@@ -22,33 +22,38 @@ export class ItemViewComponent implements OnInit {
   faDownload = faDownload;
   faPauseCircle = faPauseCircle;
   faCloudDownloadAlt = faCloudDownloadAlt;
-  
+  faPlayCircle = faPlayCircle;
+
 
   constructor(private rangeInfoService: RangeInfoService, private itemService: ItemService) { }
 
   ngOnInit(): void {
+  	const arr = JSON.parse(JSON.stringify(this.item.rangeInfo.range));
+  	arr.forEach(ls => {
+  		this.item.rangeInfo.range.push(ls);
+  	});
     this.rangeInfoService.initRangeInfo(this.item.rangeInfo);
   }
 
-  deleteItem(){
+  deleteItem() {
     return this.itemService.deleteItem(this.item.id).subscribe(deleted => {
       if (deleted) {
         this.delete.emit();
       }
     });
   }
-  
-  startItem(){
-    return this.itemService.startItem(this.item.id).subscribe(start =>{
-      if(start){
+
+  startItem() {
+    return this.itemService.startItem(this.item.id).subscribe(start => {
+      if (start) {
         console.info(`start download item ${this.item}`);
       }
     });
   }
 
-  pauseItem(){
-    return this.itemService.pauseItem(this.item.id).subscribe(pause =>{
-      if(pause){
+  pauseItem() {
+    return this.itemService.pauseItem(this.item.id).subscribe(pause => {
+      if (pause) {
         console.info(`pause download item ${this.item}`);
       }
     });
@@ -58,6 +63,11 @@ export class ItemViewComponent implements OnInit {
   itemPercent(): string {
     return `${this.rangeInfoService.percent().toFixed(3)}%`;
   }
+  
+  itemProgress(): number {
+    return Number(this.rangeInfoService.percent().toFixed(3));
+  }
+
 
   rangePercent(index: number): string {
     return `${this.rangeInfoService.rangePercent(index).toFixed(3)}%`;
@@ -71,4 +81,20 @@ export class ItemViewComponent implements OnInit {
     return `${this.rangeInfoService.rangeEndPercent(index)}%`;
   }
 
+  svgFill(index: number): string {
+    return this.rangeInfoService.isFinish(index) ? 'green' : 'blue';
+  }
+
+  getX(index: number): number {
+    return (index % 50) * 15;
+  }
+
+  getY(index: number): number {
+    return ~~(index / 50) * 15;
+  }
+  
+  getSvgHeight(): number {
+  	const rowNum = ~~(this.item?.rangeInfo?.range?.length/50) + (this.item?.rangeInfo?.range?.length%50 > 0 ? 1: 0);
+    return rowNum * 15;
+  }
 }
