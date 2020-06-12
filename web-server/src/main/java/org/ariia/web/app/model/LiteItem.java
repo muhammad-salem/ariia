e17a797f197b4f133889e59c2ed47e928900b3a4
salemebo <salem.ebo@gmail.com>
2020-06-12 09:49:01 +0200

    add range monitor

--- web-server/src/main/java/org/ariia/web/app/model/LiteItem.java
+++ web-server/src/main/java/org/ariia/web/app/model/LiteItem.java
@@ -3,21 +3,24 @@ package org.ariia.web.app.model;
 import org.ariia.core.api.writer.ItemMetaData;
 import org.ariia.items.ItemState;
 import org.ariia.range.RangeUtil;
+import org.ariia.speed.SpeedMonitor;
 
 public class LiteItem {
 	
 	public static LiteItem bind(ItemMetaData metaData) {
-		return new LiteItem(metaData.getItem().getId(),metaData.getItem().getState(), metaData.getRangeInfo());
+		return new LiteItem(metaData.getItem().getId(),metaData.getItem().getState(), metaData.getRangeInfo(), metaData.getRangeMointor());
 	}
 	
 	protected String itemId;
 	protected ItemState state;
 	protected RangeUtil rangeInfo;
-	public LiteItem(String itemId, ItemState state, RangeUtil rangeInfo) {
+	protected SpeedMonitor monitor;
+	public LiteItem(String itemId, ItemState state, RangeUtil rangeInfo, SpeedMonitor monitor) {
 		super();
 		this.itemId = itemId;
 		this.state = state;
 		this.rangeInfo = rangeInfo;
+		this.monitor = monitor;
 	}
 	
 	public String getItemId() {
