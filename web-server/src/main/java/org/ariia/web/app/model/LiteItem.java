1623ede1b09ee9120787b4daa502f694557872a9
salemebo <salem.ebo@gmail.com>
2020-06-17 10:36:26 +0200

    bind to range report

--- web-server/src/main/java/org/ariia/web/app/model/LiteItem.java
+++ web-server/src/main/java/org/ariia/web/app/model/LiteItem.java
@@ -1,6 +1,7 @@
 package org.ariia.web.app.model;
 
 import org.ariia.core.api.writer.ItemMetaData;
+import org.ariia.items.Item;
 import org.ariia.items.ItemState;
 import org.ariia.monitors.RangeReport;
 import org.ariia.range.RangeUtil;
@@ -8,26 +9,20 @@ import org.ariia.range.RangeUtil;
 public class LiteItem {
 	
 	public static LiteItem bind(ItemMetaData metaData) {
-		return new LiteItem(metaData.getItem().getId(),
-				metaData.getItem().getState(),
-				metaData.getRangeInfo(), metaData.getRangeReport());
+		return new LiteItem(metaData.getItem(), metaData.getRangeReport());
 	}
 	
 	protected String itemId;
 	protected ItemState state;
-	protected long remainingTime;
-	protected boolean downloading;
-	protected long tcpDownloadSpeed;
+	protected RangeReport report;
 	protected RangeUtil rangeInfo;
 	
-	public LiteItem(String itemId, ItemState state, RangeUtil rangeInfo, RangeReport rangeReport) {
+	public LiteItem(Item item, RangeReport report) {
 		super();
-		this.itemId = itemId;
-		this.state = state;
-		this.tcpDownloadSpeed = rangeReport.getMointor().getTcpDownloadSpeed();
-		this.remainingTime = rangeReport.getRemainingTime();
-		this.downloading = rangeReport.isDownloading();
-		this.rangeInfo = rangeInfo;
+		this.itemId = item.getId();
+		this.state = item.getState();
+		this.rangeInfo = item.getRangeInfo();
+		this.report = report;
 	}
 	
 }
