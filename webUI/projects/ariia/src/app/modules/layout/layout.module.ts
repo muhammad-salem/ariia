e081f684854733dce139bcbd1817595c3768df08
salemebo <salem.ebo@gmail.com>
2020-06-18 22:11:58 +0200

    delete shared folder

--- webUI/projects/ariia/src/app/modules/layout/layout.module.ts
+++ webUI/projects/ariia/src/app/modules/layout/layout.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -7,12 +7,11 @@ import { ThemePickerModule } from '../theme-picker/theme-picker.module';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
-import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -24,26 +23,23 @@ import { MatTreeModule } from '@angular/material/tree';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     ThemePickerModule,
+    FlexLayoutModule,
     MatDividerModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule,
-    FlexLayoutModule,
     MatMenuModule,
-    RouterModule,
     MatListModule,
-    MatTreeModule
+    MatButtonModule,
+    // MatTreeModule
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent
   ],
-  bootstrap: [
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent
-  ]
+  bootstrap: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LayoutModule { }
