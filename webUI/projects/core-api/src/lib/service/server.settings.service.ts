6a8c3a054007a94537c209dc16c8c506c3415bbf
salemebo <salem.ebo@gmail.com>
2020-06-23 22:26:52 +0200

    update setting api

--- webUI/projects/core-api/src/lib/service/server.settings.service.ts
+++ webUI/projects/core-api/src/lib/service/server.settings.service.ts
@@ -18,18 +18,17 @@ export class ServerSettingsService {
 	updateProperties(properties: Properties): Observable<boolean> {
 		return this.httpClient.post<boolean>('/settings/update', properties);
 	}
-	
+
 	startList(): Observable<boolean> {
 		return this.httpClient.post<boolean>('/settings/startList', null);
 	}
-	
+
 	pauseList(): Observable<boolean> {
 		return this.httpClient.post<boolean>('/settings/pauseList', null);
 	}
-	
-	
-	isListPaused(): Observable<boolean> {
-		return this.httpClient.get<boolean>('/settings/isListPaused');
+
+	isAllowDownload(): Observable<boolean> {
+		return this.httpClient.get<boolean>('/settings/isAllowDownload');
 	}
 
 
