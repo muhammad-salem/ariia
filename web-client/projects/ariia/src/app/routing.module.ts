8606b50012c6536e771e0372a3555ad74e0c9646
salemebo <salem.ebo@gmail.com>
2021-01-15 23:28:06 +0200

    fix routes

--- web-client/projects/ariia/src/app/routing.module.ts
+++ web-client/projects/ariia/src/app/routing.module.ts
@@ -1,38 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
 	{
-		path: 'dashboard', pathMatch: 'full',
+		path: 'dashboard',
 		loadChildren: () => import('./modules/dashboard/dashboard.module')
 			.then(module => module.DashboardModule)
 	},
 	{
-		path: 'download', pathMatch: 'full',
+		path: 'download',
 		loadChildren: () => import('./modules/downloads/downloads.module')
 			.then(module => module.DownloadsModule)
 	},
 	{
-		path: 'network', pathMatch: 'full',
+		path: 'network',
 		loadChildren: () => import('./modules/network/network.module')
 			.then(module => module.NetworkModule)
 	},
 	{
-		path: 'config', pathMatch: 'full',
+		path: 'config',
 		loadChildren: () => import('./modules/setting/setting.module')
 			.then(module => module.SettingModule)
 	},
 	{
-		path: 'logger', pathMatch: 'full',
+		path: 'logger',
 		loadChildren: () => import('./modules/logs/logs.module')
 			.then(module => module.LogsModule)
 	},
-	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-	{ path: '**', redirectTo: 'dashboard' }
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: '**', redirectTo: '/dashboard' }
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(
+		routes,
+		// { enableTracing: true }
+	)],
 	exports: [RouterModule]
 })
 export class RoutingModule {
