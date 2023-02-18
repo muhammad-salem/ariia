a51db2dcbd086761d9f047d176d1a35594c09a31
salemebo <salem.ebo@gmail.com>
2023-02-18 10:44:03 +0200

    rename classes

---
+++ cli-api/src/main/java/org/ariia/cli/LogCLI.java
@@ -0,0 +1,49 @@
+package org.ariia.cli;
+
+import org.ariia.args.Argument;
+import org.ariia.args.TerminalArgument;
+import org.ariia.logging.Log;
+import org.ariia.logging.Logger;
+import org.terminal.console.log.Level;
+import org.terminal.console.log.api.Printer;
+
+import java.util.Arrays;
+
+public class LogCLI {
+
+    private static Logger log = Logger.create(LogCLI.class);
+
+    public static void initLogServices(Argument arguments) {
+        String logLevel = arguments.getOrDefault(TerminalArgument.Debug, Level.info.name());
+        Log.level(logLevel);
+        Log.initService();
+        log.trace( "Terminal Argument", Arrays.toString(arguments.getArgs()));
+    }
+
+    public static void initLogServices(Argument arguments, Level defaultLevel) {
+        String logLevel = arguments.getOrDefault(TerminalArgument.Debug, defaultLevel.name());
+        Log.level(logLevel);
+        Log.initService();
+        log.trace("Terminal Argument", Arrays.toString(arguments.getArgs()));
+    }
+
+    public static void initLogServices(Argument arguments, Printer printer, Level defaultLevel) {
+        String logLevel = arguments.getOrDefault(TerminalArgument.Debug, defaultLevel.name());
+        Log.printer(printer, logLevel);
+        Log.initService();
+        log.trace("Terminal Argument", Arrays.toString(arguments.getArgs()));
+
+    }
+
+    public static void initLogServicesNoStart(Argument arguments, Printer printer, Level defaultLevel) {
+        String logLevel = arguments.getOrDefault(TerminalArgument.Debug, defaultLevel.name());
+        Log.printer(printer, logLevel);
+        log.trace("Terminal Argument", Arrays.toString(arguments.getArgs()));
+    }
+
+    public static void startLogService() {
+        Log.initService();
+    }
+
+
+}
--- core-api/src/main/java/org/ariia/core/api/queue/BackwardOrder.java
+++ core-api/src/main/java/org/ariia/core/api/queue/BackwardOrder.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public interface BackwordOrder {
+public interface BackwardOrder {
 
     default List<Integer> streamDownloadOrder(int count) {
         if (count == 0) return Collections.emptyList();
