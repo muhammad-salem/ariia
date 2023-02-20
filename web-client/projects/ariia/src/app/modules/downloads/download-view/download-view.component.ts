87e045680c8582c8b8b6880bd5b20ba7b81b031d
salemebo <salem.ebo@gmail.com>
2023-02-20 20:49:42 +0200

    editItem

--- web-client/projects/ariia/src/app/modules/downloads/download-view/download-view.component.ts
+++ web-client/projects/ariia/src/app/modules/downloads/download-view/download-view.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService, Item, ItemService, RangeService } from 'core-api';
 import { NotifyService } from '../../layout/app-root/notify.service';
+import { DownloadEditComponent } from '../download-edit/download-edit.component';
 
 @Component({
 	selector: 'download-view',
@@ -15,9 +17,14 @@ export class DownloadViewComponent implements OnInit {
 
 	errorMessage: string;
 
-	constructor(private dataService: DataService, private itemService: ItemService,
-				private rangeService: RangeService, private notifyService: NotifyService,
-				private route: ActivatedRoute, private router: Router) { }
+	constructor(
+		private dataService: DataService,
+		private itemService: ItemService,
+		private rangeService: RangeService,
+		private notifyService: NotifyService,
+		private dialog: MatDialog,
+		private route: ActivatedRoute,
+		private router: Router) { }
 
 	ngOnInit(): void {
 		const id = +this.route.snapshot.params.id;
@@ -49,6 +56,19 @@ export class DownloadViewComponent implements OnInit {
 		});
 	}
 
+	editItem(): void {
+		const dialogRef = this.dialog.open(DownloadEditComponent, {
+			data: {
+				url: this.item.url,
+				filename: this.item.filename,
+				saveDirectory: this.item.saveDirectory,
+				headers: this.item.headers,
+				redirectUrl: this.item.redirectUrl,
+			},
+		});
+		dialogRef.afterClosed().subscribe((item: Item) => this.itemService.updateItem(this.item.id, item).subscribe());
+	}
+
 	downloadPercent(): string {
 		return `${this.percent()}%`;
 	}
@@ -74,7 +94,7 @@ export class DownloadViewComponent implements OnInit {
 	}
 
 	getSvgHeight(): number {
-		const rowNum = ~~(this.item?.rangeInfo?.range?.length/50) + (this.item?.rangeInfo?.range?.length % 50 > 0 ? 1 : 0);
+		const rowNum = ~~(this.item?.rangeInfo?.range?.length / 50) + (this.item?.rangeInfo?.range?.length % 50 > 0 ? 1 : 0);
 		return rowNum * 15;
 	}
 
