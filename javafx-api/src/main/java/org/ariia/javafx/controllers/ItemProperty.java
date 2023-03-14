41967e16ce583046a743603db2e8763b2f6b6071
salemebo <salem.ebo@gmail.com>
2023-03-14 21:24:36 +0200

    add ItemProperty

---
+++ javafx-api/src/main/java/org/ariia/javafx/controllers/ItemProperty.java
@@ -0,0 +1,40 @@
+package org.ariia.javafx.controllers;
+
+import javafx.beans.property.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.ariia.items.Item;
+import org.ariia.monitors.RangeReport;
+
+@Getter
+@RequiredArgsConstructor
+public class ItemProperty {
+
+    public static ItemProperty of(Item item, RangeReport monitor) {
+        return new ItemProperty(item, monitor);
+    }
+
+    private final Item item;
+    private final RangeReport monitor;
+
+    private IntegerProperty id = new SimpleIntegerProperty();
+    private StringProperty name = new SimpleStringProperty();
+    private StringProperty url = new SimpleStringProperty();
+    private StringProperty status = new SimpleStringProperty();
+    private StringProperty length = new SimpleStringProperty();
+    private StringProperty progress = new SimpleStringProperty();
+    private StringProperty speed = new SimpleStringProperty();
+    private StringProperty timeLeft = new SimpleStringProperty();
+
+    public void updateMonitoring(){
+        id.setValue(item.getId());
+        name.set(item.getFilename());
+        url.set(item.getUrl());
+        status.set(item.getState().toString());
+        length.set(monitor.getDownloadLength());
+        progress.set(monitor.getPercent());
+        speed.set(monitor.getTcpDownloadSpeed());
+        timeLeft.set(monitor.getRemainingTimeString());
+    }
+
+}
