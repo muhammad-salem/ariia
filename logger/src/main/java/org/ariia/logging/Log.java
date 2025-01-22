2b4132266dde65b30f9fa3965e972ebd9c92b50d
muhammad-salem <salem.ebo@gmail.com>
2025-01-22 04:07:54 +0200

    refactor class Log

--- logger/src/main/java/org/ariia/logging/Log.java
+++ logger/src/main/java/org/ariia/logging/Log.java
@@ -5,42 +5,40 @@ import org.terminal.console.log.api.Printer;
 import org.terminal.console.log.impl.Message;
 import org.terminal.console.log.impl.PrinterImpl;
 
-import java.util.Arrays;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public final class Log {
     private static Level level = Level.info;
     private static Printer printer = new PrinterImpl(System.out);
-    private static ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
-    private static ScheduledExecutorService ex = new ScheduledThreadPoolExecutor(1);
+    private static final ConcurrentLinkedQueue<Message> MESSAGES = new ConcurrentLinkedQueue<>();
+    private static final ScheduledExecutorService SCHEDULED_EXECUTOR_SERVICE = new ScheduledThreadPoolExecutor(1, Thread.ofVirtual().factory());
 
     private Log() {
     }
 
     public static void stopLogging() {
-        if (!queue.isEmpty()) {
+        if (!MESSAGES.isEmpty()) {
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        ex.shutdownNow();
-        queue.clear();
+        SCHEDULED_EXECUTOR_SERVICE.shutdownNow();
+        MESSAGES.clear();
     }
 
     public static void initService() {
-        ex.scheduleAtFixedRate(Log::scheduleLoggingTask, 0, 100, TimeUnit.MILLISECONDS);
+        SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(Log::scheduleLoggingTask, 0, 100, TimeUnit.MILLISECONDS);
     }
 
     private static void scheduleLoggingTask() {
-        while (!queue.isEmpty()) {
+        while (!MESSAGES.isEmpty()) {
             try {
-                Message message = queue.remove();
+                Message message = MESSAGES.remove();
                 printer.print(message);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -80,211 +78,211 @@ public final class Log {
 
     public static void log(String message) {
 //		if (isAllowed(Level.log)) {
-        queue.add(new Message(Level.log, message));
+        MESSAGES.add(new Message(Level.log, message));
 //		}
     }
 
     public static void error(String message) {
         if (isAllowed(Level.error)) {
-            queue.add(new Message(Level.error, message));
+            MESSAGES.add(new Message(Level.error, message));
         }
     }
 
     public static void warn(String message) {
         if (isAllowed(Level.warn)) {
-            queue.add(new Message(Level.warn, message));
+            MESSAGES.add(new Message(Level.warn, message));
         }
     }
 
     public static void info(String message) {
         if (isAllowed(Level.info)) {
-            queue.add(new Message(Level.info, message));
+            MESSAGES.add(new Message(Level.info, message));
         }
     }
 
     public static void assertions(Boolean assertions, String message) {
         if (!assertions) {
-            queue.add(new Message(Level.assertion, message));
+            MESSAGES.add(new Message(Level.assertion, message));
         }
     }
 
     public static void debug(String message) {
         if (isAllowed(Level.debug)) {
-            queue.add(new Message(Level.debug, message));
+            MESSAGES.add(new Message(Level.debug, message));
         }
     }
 
     public static void trace(String message) {
         if (isAllowed(Level.trace)) {
-            queue.add(new Message(Level.trace, message));
+            MESSAGES.add(new Message(Level.trace, message));
         }
     }
 
     public static void log(Class<?> classname, String message) {
 //		if (isAllowed(Level.log)) {
-        queue.add(new Message(Level.log, classname, message));
+        MESSAGES.add(new Message(Level.log, classname, message));
 //		}
     }
 
     public static void error(Class<?> classname, String message) {
         if (isAllowed(Level.error)) {
-            queue.add(new Message(Level.error, classname, message));
+            MESSAGES.add(new Message(Level.error, classname, message));
         }
     }
 
     public static void warn(Class<?> classname, String message) {
         if (isAllowed(Level.warn)) {
-            queue.add(new Message(Level.warn, classname, message));
+            MESSAGES.add(new Message(Level.warn, classname, message));
         }
     }
 
     public static void info(Class<?> classname, String message) {
         if (isAllowed(Level.info)) {
-            queue.add(new Message(Level.info, classname, message));
+            MESSAGES.add(new Message(Level.info, classname, message));
         }
     }
 
     public static void assertions(Boolean assertions, Class<?> classname, String message) {
         if (!assertions) {
-            queue.add(new Message(Level.assertion, classname, message));
+            MESSAGES.add(new Message(Level.assertion, classname, message));
         }
     }
 
     public static void debug(Class<?> classname, String message) {
         if (isAllowed(Level.debug)) {
-            queue.add(new Message(Level.debug, classname, message));
+            MESSAGES.add(new Message(Level.debug, classname, message));
         }
     }
 
     public static void trace(Class<?> classname, String message) {
         if (isAllowed(Level.trace)) {
-            queue.add(new Message(Level.trace, classname, message));
+            MESSAGES.add(new Message(Level.trace, classname, message));
         }
     }
 
     public static void log(String title, String message) {
 //		if (isAllowed(Level.log)) {
-        queue.add(new Message(Level.log, title, message));
+        MESSAGES.add(new Message(Level.log, title, message));
 //		}
     }
 
     public static void error(String title, String message) {
         if (isAllowed(Level.error)) {
-            queue.add(new Message(Level.error, title, message));
+            MESSAGES.add(new Message(Level.error, title, message));
         }
     }
 
     public static void warn(String title, String message) {
         if (isAllowed(Level.warn)) {
-            queue.add(new Message(Level.warn, title, message));
+            MESSAGES.add(new Message(Level.warn, title, message));
         }
     }
 
     public static void info(String title, String message) {
         if (isAllowed(Level.info)) {
-            queue.add(new Message(Level.info, title, message));
+            MESSAGES.add(new Message(Level.info, title, message));
         }
     }
 
     public static void assertions(Boolean assertions, String title, String message) {
         if (!assertions) {
-            queue.add(new Message(Level.assertion, title, message));
+            MESSAGES.add(new Message(Level.assertion, title, message));
         }
     }
 
     public static void debug(String title, String message) {
         if (isAllowed(Level.debug)) {
-            queue.add(new Message(Level.debug, title, message));
+            MESSAGES.add(new Message(Level.debug, title, message));
         }
     }
 
     public static void trace(String title, String message) {
         if (isAllowed(Level.trace)) {
-            queue.add(new Message(Level.trace, title, message));
+            MESSAGES.add(new Message(Level.trace, title, message));
         }
     }
 
     public static void log(Class<?> classname, String title, String message) {
 //		if (isAllowed(Level.log)) {
-        queue.add(new Message(Level.log, classname, title, message));
+        MESSAGES.add(new Message(Level.log, classname, title, message));
 //		}
     }
 
     public static void error(Class<?> classname, String title, String message) {
         if (isAllowed(Level.error)) {
-            queue.add(new Message(Level.error, classname, title, message));
+            MESSAGES.add(new Message(Level.error, classname, title, message));
         }
     }
 
     public static void warn(Class<?> classname, String title, String message) {
         if (isAllowed(Level.warn)) {
-            queue.add(new Message(Level.warn, classname, title, message));
+            MESSAGES.add(new Message(Level.warn, classname, title, message));
         }
     }
 
     public static void info(Class<?> classname, String title, String message) {
         if (isAllowed(Level.info)) {
-            queue.add(new Message(Level.info, classname, title, message));
+            MESSAGES.add(new Message(Level.info, classname, title, message));
         }
     }
 
     public static void assertions(Boolean assertions, Class<?> classname, String title, String message) {
         if (!assertions) {
-            queue.add(new Message(Level.assertion, classname, title, message));
+            MESSAGES.add(new Message(Level.assertion, classname, title, message));
         }
     }
 
     public static void debug(Class<?> classname, String title, String message) {
         if (isAllowed(Level.debug)) {
-            queue.add(new Message(Level.debug, classname, title, message));
+            MESSAGES.add(new Message(Level.debug, classname, title, message));
         }
     }
 
     public static void trace(Class<?> classname, String title, String message) {
         if (isAllowed(Level.trace)) {
-            queue.add(new Message(Level.trace, classname, title, message));
+            MESSAGES.add(new Message(Level.trace, classname, title, message));
         }
     }
 
     public static void log(Class<?> classname, String title, String... message) {
 //		if (isAllowed(Level.log)) {
-        queue.add(new Message(Level.log, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+        MESSAGES.add(new Message(Level.log, classname, title, String.join("\n", message)));
 //		}
     }
 
     public static void error(Class<?> classname, String title, String... message) {
         if (isAllowed(Level.error)) {
-            queue.add(new Message(Level.error, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+            MESSAGES.add(new Message(Level.error, classname, title, String.join("\n", message)));
         }
     }
 
     public static void warn(Class<?> classname, String title, String... message) {
         if (isAllowed(Level.warn)) {
-            queue.add(new Message(Level.warn, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+            MESSAGES.add(new Message(Level.warn, classname, title, String.join("\n", message)));
         }
     }
 
     public static void info(Class<?> classname, String title, String... message) {
         if (isAllowed(Level.info)) {
-            queue.add(new Message(Level.info, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+            MESSAGES.add(new Message(Level.info, classname, title, String.join("\n", message)));
         }
     }
 
     public static void assertions(Boolean assertions, Class<?> classname, String title, String... message) {
         if (!assertions) {
-            queue.add(new Message(Level.assertion, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+            MESSAGES.add(new Message(Level.assertion, classname, title, String.join("\n", message)));
         }
     }
 
     public static void debug(Class<?> classname, String title, String... message) {
         if (isAllowed(Level.debug)) {
-            queue.add(new Message(Level.debug, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+            MESSAGES.add(new Message(Level.debug, classname, title, String.join("\n", message)));
         }
     }
 
     public static void trace(Class<?> classname, String title, String... message) {
         if (isAllowed(Level.trace)) {
-            queue.add(new Message(Level.trace, classname, title, Arrays.asList(message).stream().collect(Collectors.joining("\n"))));
+            MESSAGES.add(new Message(Level.trace, classname, title, String.join("\n", message)));
         }
     }
 }
